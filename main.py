bb4fd784a6064adf8d36acf59c07d4250ab2d0b7
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2024-06-21 03:02:20 +0900

    wandb init

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 from thop import profile, clever_format
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import wandb
 
 import utils
 from model import Model
@@ -95,11 +96,31 @@ if __name__ == '__main__':
     parser.add_argument('--k', default=200, type=int, help='Top k most similar images used to predict the label')
     parser.add_argument('--batch_size', default=512, type=int, help='Number of images in each mini-batch')
     parser.add_argument('--epochs', default=500, type=int, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--lr', default=1e-3, type=float, help='Learning Rate at the training start')
+    parser.add_argument('--weight_decay', default=1e-6, type=float, help='Weight Decay')
+    parser.add_argument('--dataset', default='stl10', type=str, help='Training Dataset (e.g. CIFAR10, STL10)')
+    parser.add_argument('--wandb_project', default='default_project', type=str, help='WandB Project name')
+    parser.add_argument('--wandb_run', default='default_run', type=str, help='WandB run name')
 
     # args parse
     args = parser.parse_args()
     feature_dim, temperature, k = args.feature_dim, args.temperature, args.k
     batch_size, epochs = args.batch_size, args.epochs
+
+    # wandb init
+    config = {
+        "lr": args.lr,
+        "weight_decay": args.weight_decay,
+        "arch": "resnet50",
+        "dataset": args.dataset,
+        "epochs": args.epochs,
+        "batch_size": args.batch_size,
+        "momentum": args.momentum,
+        "temperature": args.temperature,
+        "feature_dim": args.feature_dim,
+        "queue_size": args.m
+    }
+    wandb.init(project=args.wandb_project, name=args.wandb_run, config=config)
 
     # data prepare
     train_data = utils.CIFAR10Pair(root='data', train=True, transform=utils.train_transform, download=True)
@@ -130,9 +151,14 @@ if __name__ == '__main__':
         test_acc_1, test_acc_5 = test(model, memory_loader, test_loader)
         results['test_acc@1'].append(test_acc_1)
         results['test_acc@5'].append(test_acc_5)
+        # save statistics to wandb
+        wandb.log({'train_loss': train_loss, 'test_acc@1': test_acc_1, 'test_acc@5': test_acc_5})
         # save statistics
         data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
         data_frame.to_csv('results/{}_statistics.csv'.format(save_name_pre), index_label='epoch')
         if test_acc_1 > best_acc:
             best_acc = test_acc_1
             torch.save(model.state_dict(), 'results/{}_model.pth'.format(save_name_pre))
+
+    wandb.save('results/{}_model.pth'.format(save_name_pre))
+    wandb.finish()
